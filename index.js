ab16de6bb920fc4dd194800da13e5f4a20a25c87
Jack Kuhry <jackkuhry@Jacks-MacBook-Air.local>
2016-06-17 09:53:46 -0700

    ChangeReview

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 $(function() {
-    $("#yourBtn").on("click", function(){
-      
-      var duecost = $("#howMuchdue").val();
+    $("#yourBtn").on("click", function(){  //this is the button process once clicked.  
+
+      var duecost = $("#howMuchdue").val(); //getting user input.
       var cashGiven = $("#cusGive").val();
 
 
-      duecost = duecost*100;
-      cashGiven = cashGiven*100;
+      dueCost = dueCost*100;
+      cashGiven = cashGiven*100;   //start of calculation.
 
 
-      var change = cashGiven-duecost;
+      var change = cashGiven-dueCost;
       var dollars = Math.floor(change/100);
 
-      change = change%100;
-      //change = change - 100*dollars;
-
+      change = change%100;   
 
       var quarters = Math.floor(change/25);
       change = change%25;
@@ -25,9 +23,9 @@ $(function() {
        var nickels = Math.floor(change/5);
       change = change%5;
 
-      var pennies = change;
+      var pennies = change;     
 
-      $("#dollars").text(dollars);
+      $("#dollars").text(dollars);  //this is my output data.
       $("#quarters").text(quarters);
       $("#dimes").text(dimes);
       $("#nickels").text(nickels);
@@ -36,4 +34,11 @@ $(function() {
 
     })
 });
+
+//fixed my comments.  MS/SB/JK
+
+
+
+
     
+
